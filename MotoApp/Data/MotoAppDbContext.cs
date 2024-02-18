b6bc61a8e3f133c5fd3204bb6ac3552acf3fd491
sebekoo <sebekoo@o2.pl>
2024-02-18 18:51:32 +0100

    Tworzenie DbContext

---
+++ MotoApp/Data/MotoAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotoApp.Entities;
+
+namespace MotoApp.Data;
+
+public class MotoAppDbContext : DbContext
+{
+    public DbSet<Employee> Employees => Set<Employee>();
+
+    public DbSet<BusinessPartner> BusinessPartner => Set<BusinessPartner>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseInMemoryDatabase("StorageAppDb");
+    }
+}
