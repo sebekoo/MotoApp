d2f2ea5694290e12fd2589f1d2d65a5e52d5f484
sebekoo <sebekoo@o2.pl>
2024-02-18 16:37:02 +0100

    First Repository

---
+++ MotoApp/Repositories/EmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace MotoApp.Repositories;
+
+using MotoApp.Entities;
+
+public class EmployeeRepository
+{
+    private readonly List<Employee> _employees = [];
+
+    public void Add(Employee employee)
+    {
+        employee.Id = _employees.Count + 1;
+        _employees.Add(employee);
+    }
+
+    public void Save()
+    {
+        foreach (var employee in _employees)
+        {
+            Console.WriteLine(employee);
+        }
+    }
+
+    public Employee GetById(int id)
+    {
+        return _employees.Single(item => item.Id == id);
+    }
+}
