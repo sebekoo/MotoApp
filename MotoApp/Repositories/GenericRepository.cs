12aa956619cd287b0930322e510de42e20437461
sebekoo <sebekoo@o2.pl>
2024-02-18 16:52:45 +0100

    Repozytorium generyczne

---
+++ MotoApp/Repositories/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace MotoApp.Repositories;
+
+using MotoApp.Entities;
+
+public class GenericRepository<T>
+{
+    private readonly List<T> _items = [];
+
+    public void Add(T item)
+    {
+        //item.Id = _items.Count + 1;
+        _items.Add(item);
+    }
+
+    public void Save()
+    {
+        foreach (var item in _items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public T GetById(int id)
+    {
+        return _items.Single(item => item.Id == id);
+    }
+}
