b4cef5cbbd8eda4fc0399a0f8de2583349c8bdda
sebekoo <sebekoo@o2.pl>
2024-02-18 18:18:53 +0100

    Ograniczenie new()

--- MotoApp/Entities/Employee.cs
+++ MotoApp/Entities/Employee.cs
@@ -2,6 +2,14 @@
 
 public class Employee : EntityBase
 {
+    public Employee()
+    {
+    }
+
+    public Employee(string name)
+    {
+    }
+
     public string? FirstName { get; set; }
 
     public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
--- MotoApp/Repositories/GenericRepository.cs
+++ MotoApp/Repositories/GenericRepository.cs
@@ -3,11 +3,16 @@
 using MotoApp.Entities;
 
 public class GenericRepository<TEntity, TKey>
-    where TEntity : class, IEntity
+    where TEntity : class, IEntity, new()
     where TKey : struct
 {
     public TKey? Key { get; set; }
     protected readonly List<TEntity> _items = [];
+
+    public TEntity CreatenewItem()
+    {
+        return new TEntity();
+    }
 
     public void Add(TEntity item)
     {
