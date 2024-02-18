ec8a31c19f413f84f5e4ae10a79694b822055ed4
sebekoo <sebekoo@o2.pl>
2024-02-18 16:45:43 +0100

    Praca ze wzorcem repozytorium

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MotoApp.Entities;
+using MotoApp.Repositories;
+
+var employeeRepository = new EmployeeRepository();
+employeeRepository.Add(new Employee { FirstName = "Adam" });
+employeeRepository.Add(new Employee { FirstName = "Piotr" });
+employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+employeeRepository.Save();
