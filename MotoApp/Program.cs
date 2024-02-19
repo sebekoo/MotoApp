26cda1b0da342fe6e52351dbdc6a1cd2fbeb5315
sebekoo <sebekoo@o2.pl>
2024-02-19 19:29:35 +0100

    Batch oraz.....

--- MotoApp/Program.cs
+++ MotoApp/Program.cs
@@ -4,23 +4,42 @@ using MotoApp.Repositories;
 
 var employeeRepository = new SqlRepository<Employee>(new MotoAppDbContext());
 AddEmployees(employeeRepository);
-AddManagers(employeeRepository);
+//AddManagers(employeeRepository);
 WriteAllToConsole(employeeRepository);
 
 static void AddEmployees(IRepository<Employee> employeeRepository)
 {
-    employeeRepository.Add(new Employee { FirstName = "Adam" });
-    employeeRepository.Add(new Employee { FirstName = "Piotr" });
-    employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+    var employees = new List<Employee>()
+    {
+        new Employee { FirstName = "Adam" },
+        new Employee { FirstName = "Piotr" },
+        new Employee { FirstName = "Zuzia" }
+    };
+
+    AddBatch(employeeRepository, employees);
+
+    //employeeRepository.Add(new Employee { FirstName = "Adam" });
+    //employeeRepository.Add(new Employee { FirstName = "Piotr" });
+    //employeeRepository.Add(new Employee { FirstName = "Zuzia" });
+    //employeeRepository.Save();
+}
+
+static void AddBatch(IRepository<Employee> employeeRepository, List<Employee> employees)
+{
+    foreach (var employee in employees)
+    {
+        employeeRepository.Add(employee);
+    }
+
     employeeRepository.Save();
 }
 
-static void AddManagers(IWriteRepository<Manager> managerRepository)
-{
-    managerRepository.Add(new Manager { FirstName = "Przemek" });
-    managerRepository.Add(new Manager { FirstName = "Tomek" });
-    managerRepository.Save();
-}
+//static void AddManagers(IWriteRepository<Manager> managerRepository)
+//{
+//    managerRepository.Add(new Manager { FirstName = "Przemek" });
+//    managerRepository.Add(new Manager { FirstName = "Tomek" });
+//    managerRepository.Save();
+//}
 
 static void WriteAllToConsole(IReadRepository<IEntity> repository)
 {
